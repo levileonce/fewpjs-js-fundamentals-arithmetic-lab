bd13caf5ebbe40212daa5f568339a9b4f2edf192
Levi <leviedgeleonce@gmail.com>
2021-04-07 14:35:28 -0700

    commit tree 6aa9f0e68b8a5ee934b93c2e992b8b4baf7881dc

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
 
 // Write your code here
+function mulitiply(num1,num2) {
+}
+mulitiply(31,2)
+
+function max(set) {
+    return Math.max(1,2,3,4,7,8,9,20);
+}
+max()
+
+// function random() {
+//      Math.random() 
+// }
+
+
+// Number.isInteger()
+// Checks whether the provided argument is an integer:
+
+// Number.isInteger(42);
+// //=> true
+
+// Number.isInteger(0.42);
+// //=> false 
+
+
+
+
+// describe('index.js', function() {
+//     describe('multiply', function() {
+//       it("is an equation whose multiplied results will equal 62", function() {
+//         expect(num1).to.be.a('number')
+//         expect(num2).to.be.a('number')
+//         expect(multiply).to.eq(62)
+//       });
+//     });
+//     describe('random', function() {
+//       it("generates a random integer greater than 0", function() {
+//         expect(Number.isInteger(random)).to.be.true
+//         expect(random).to.not.eq(0)
+//       });
+//     });
+//     describe('mod', function() {
+//       it("is an equation that calculates a remainder that is equal to 4", function() {
+//         expect(num3).to.be.a('number')
+//         expect(num4).to.be.a('number')
+//         expect(mod).to.eq(4)
+//       });
+//     });
+//     // describe('max', function() {
+//       it("will return 20 as the highest number in the set", function() {
+//         expect(max).to.eq(20)
+//       });
+//     });
+//   });
